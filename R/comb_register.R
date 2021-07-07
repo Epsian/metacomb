bb714da510ad33c8a83d0a001d09700a4931d318
Jared Joseph <jared.n.joseph@gmail.com>
2021-07-07 15:47:03 -0700

    added option to set location

--- R/comb_register.R
+++ R/comb_register.R
@@ -1,6 +1,6 @@
 # function to register the current file into the metacomb database
 
-comb_register = function(file_id, type = "script"){
+comb_register = function(file_id, type = "script", location = NA){
 
   # connect to metacomb db
   .comb_db = metacomb::comb_connect()
@@ -9,8 +9,8 @@ comb_register = function(file_id, type = "script"){
   on.exit(DBI::dbClearResult(.comb_db))
   on.exit(DBI::dbDisconnect(.comb_db))
 
-  # find file location
-  location = ifelse(type == "script", metacomb::comb_script(), NA)
+  # find file location if NA
+  if(is.na(location)){location = ifelse(type == "script", metacomb::comb_script(), NA)}
 
   # make info to input
   file_meta = paste("file_id" = paste0("'", file_id, "'"),
@@ -20,7 +20,7 @@ comb_register = function(file_id, type = "script"){
 
   # now try to enter the info
   out = DBI::dbExecute(.comb_db, paste0("
-                       INSERT OR REPLACE INTO file_meta (file_id, file_location, last_updater)
+                       INSERT OR REPLACE INTO file_meta (file_id, file_location, file_type, last_updater)
                        VALUES (", file_meta,")
                        "))
 

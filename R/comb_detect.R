f34e7e31a77930a0cee78a766f1425be512ce0ea
Jared Joseph <jared.n.joseph@gmail.com>
2021-07-13 11:34:45 -0700

    finished in/out detect function

--- R/comb_detect.R
+++ R/comb_detect.R
@@ -1,71 +1,70 @@
-# to detect all input/output files in a script
+# to detect all input/output files in a script and return them if they are valid WITHIN THIS PROJECT
 
-# arguments
-location = "G:/Projects/work/h4ca_cartogram/src/carto.R"
-inout_functions = c("read.csv", "write.csv", "readRDS", "saveRDS", "tmap_save")
+comb_detect = function(location, direction, custom_functions = NULL){
 
-# load in script
-script = readLines(location)
+  # set if looking for in or out functions
+  if(direction == "in"){inout_functions = c("read.csv", "readRDS", custom_functions)} else
+  if(direction == "out"){inout_functions = c("write.csv", "saveRDS", "tmap_save", custom_functions)} else
+  {stop("Invalid direction, pleae use 'in' or 'out'.")}
 
-# remove all comments
-script = gsub("#.+$", "", script)
+  # load in script
+  script = readLines(location)
 
-# remove empties
-script = script[script != ""]
+  # remove all comments
+  script = gsub("#.+$", "", script)
 
+  # remove empties
+  script = script[script != ""]
 
-# combine lines with pipe-likes
-# start with the last line and work up
-for(i in length(script):2){
+  # combine lines with pipe-likes
+  # start with the last line and work up
+  for(i in length(script):2){
 
-  # combine lines if line ends with +, %>%, or |>
+    # combine lines if line ends with +, %>%, or |>
 
-  # does this like end with a trailing pipe-like?
-  if(grepl("(?:%>%)\\s*$|(?:\\+)\\s*$|(?:\\|>)\\s*$", script[i-1])){
+    # does this like end with a trailing pipe-like?
+    if(grepl("(?:%>%)\\s*$|(?:\\+)\\s*$|(?:\\|>)\\s*$", script[i-1])){
 
-    # then combine with next line
-    script[i-1] = paste0(script[i-1], script[i], collapse = " ")
+      # then combine with next line
+      script[i-1] = paste0(script[i-1], script[i], collapse = " ")
 
-    # NA out current line
-    script[i] = NA
+      # NA out current line
+      script[i] = NA
+
+    }
+
+    # does this like end with a starting pipe-like?
+    if(grepl("^\\s*(?:%>%)|^\\s*(?:\\+)|^\\s*(?:\\|>)", script[i])){
+
+      # then combine with next line
+      script[i-1] = paste0(script[i-1], script[i], collapse = " ")
+
+      # NA out current line
+      script[i] = NA
+
+    }
 
   }
 
-  # does this like end with a starting pipe-like?
-  if(grepl("^\\s*(?:%>%)|^\\s*(?:\\+)|^\\s*(?:\\|>)", script[i])){
+  # remove new NAs
+  script = script[!is.na(script)]
 
-    # then combine with next line
-    script[i-1] = paste0(script[i-1], script[i], collapse = " ")
+  # keep only lines that have a in/out function as the vector above
+  script = script[which(as.logical(rowSums(sapply(inout_functions, grepl, x = script))))]
 
-    # NA out current line
-    script[i] = NA
+  # get everything in single or double quotes (possible paths)
+  quotes = unlist(stringr::str_extract_all(script, "\".*?\"|\'.*?\'"))
 
-  }
+  # clean out quotes
+  quotes = unname(sapply(quotes, gsub, pattern = '\"', replacement = ''))
+
+  # keep only valid path in this project
+  paths = quotes[sapply(quotes, FUN = function(item){file.exists(item) | dir.exists(item)})]
+
+  # return
+  if(length(paths) == 0){return(NA)} else {return(paths)}
 
 }
-
-# remove new NAs
-script = script[!is.na(script)]
-
-# keep only lines that have a in/out function as the vector above
-script = script[which(as.logical(rowSums(sapply(inout_functions, grepl, x = script))))]
-
-# get everything in single or double quotes (possible paths)
-quotes = unlist(stringr::str_extract_all(script, "\".*?\"|\'.*?\'"))
-
-# clean out quotes
-quotes = sapply(quotes, gsub, pattern = '\"', replacement = '')
-
-# check if valid path in this project
-
-file = quotes[1]
-
-if(file(file, "wb")){on.exit(close(con))} else if (inherits(file, "connection")){con <- file} else stop("bad file argument")
-
-
-
-
-
 
 
 

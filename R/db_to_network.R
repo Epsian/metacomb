e2a3c91c546757ee0406d8cfc6f40d6ce537cce8
Jared Joseph <jared.n.joseph@gmail.com>
2021-08-30 15:22:50 -0700

    added hierarchical plotting (#3)
    * fixed name error
    
    * added level option for later plotting
    
    * added basic hierarchical plotting

--- R/db_to_network.R
+++ R/db_to_network.R
@@ -1,6 +1,6 @@
 # To make a network dataframe for vis from database
 
-comb_network = function(){
+comb_network = function(hierarchical = FALSE){
 
   # setup ####
 
@@ -24,7 +24,7 @@ comb_network = function(){
   ## nodes ####
 
   # add files in edges that are not in nodes
-  edge_nodes = data.frame("file_id" = NA, "file_location" = unique(c(edges$from_file_id, edges$to_file_id)), "file_type" = "other", "last_update" = NA, "last_updater" = NA)
+  edge_nodes = data.frame("file_id" = NA, "file_location" = unique(c(edges$from_file_id, edges$to_file_id)), "file_type" = "other", "file_level" = NA, "last_update" = NA, "last_updater" = NA)
 
   # remove rows with known ids
   edge_nodes = edge_nodes[!(edge_nodes$file_location %in% nodes$file_id), ]
@@ -62,16 +62,52 @@ comb_network = function(){
   # add arrows
   edges$arrows = "to"
 
+  ## make network ####
+
+  cgraph = igraph::graph_from_data_frame(edges, directed = TRUE, vertices = nodes)
+
+  ## hierarchical plotting ####
+
+  if(hierarchical == TRUE){
+
+    # rename file_level for hierarchical plotting
+    colnames(nodes)[colnames(nodes) == "file_level"] = "level"
+
+    # make string NAs real NAs
+    nodes$level[nodes$level == "NA"] = NA
+
+    # get the max inward geodesic distances for each node
+    cdist = igraph::distances(cgraph, mode = "in")
+    cdist[is.infinite(cdist)] = NA
+    nodes$in_dist = (apply(cdist, 1, max, na.rm = TRUE) + 1) # add 1 for 0s
+
+    # put back in network
+    cgraph = igraph::set_vertex_attr(cgraph, "in_dist", value = nodes$in_dist)
+
+    # add level in is NA, keep existing (from manual assignment)
+    nodes$level = ifelse(is.na(nodes$level), nodes$in_dist, nodes$level)
+
+    # assign back to network
+    cgraph = igraph::set_vertex_attr(cgraph, "level", value = nodes$level)
+
+  }
+
   ## plot ####
 
+  # take network back to visnetwork format
+  plot_list = visNetwork::toVisNetworkData(cgraph)
+
   # make network
-  out_net = visNetwork::visOptions(visNetwork::visNetwork(nodes, edges), highlightNearest = TRUE)
+  out_net = visNetwork::visOptions(visNetwork::visNetwork(plot_list$nodes, plot_list$edges), highlightNearest = TRUE)
 
   # set to fill page
   out_net$sizingPolicy$browser$fill = TRUE
 
   # add legend
   out_net = visNetwork::visLegend(out_net, main = "File Type")
+
+  # set hierarchical
+  if(hierarchical == TRUE){out_net = visNetwork::visHierarchicalLayout(out_net, direction = "LR")}
 
   # output
   return(out_net)
